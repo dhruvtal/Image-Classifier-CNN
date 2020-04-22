680a65133c230754c3a92b396920f641b847772d
dhruvtal <64063264+dhruvtal@users.noreply.github.com>
2020-04-22 09:01:27 +0530

    Add files via upload

---
+++ Dog_Cat_Classifier.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 08:00:36 2020
+
+@author: dhruv
+"""
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.optimizers import SGD 
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Model
+from keras import backend as K
+from keras.utils import np_utils
+from keras.optimizers import RMSprop
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
+import util
+from sklearn.metrics import confusion_matrix
+train_df = pd.read_csv("train.csv")
+valid_df = pd.read_csv("valid.csv")
+test_df = pd.read_csv("test.csv")
+
+
+labels = ['Group']
+ 
+def get_train_generator(df, image_dir, x_col, y_cols, shuffle=True, batch_size=8, seed=1, target_w = 320, target_h = 320):
+         
+    # normalize images
+    image_generator = ImageDataGenerator(
+        samplewise_center=True,
+        samplewise_std_normalization= True)
+    
+    # Image flow from directory with batch size output image size
+    generator = image_generator.flow_from_dataframe(dataframe=df,
+            directory=image_dir,
+            x_col=x_col,
+            y_col=y_cols,
+            class_mode="raw",
+            batch_size=batch_size,
+            seed=seed,
+            target_size=(target_w,target_h))
+    
+    return generator
+
+def get_test_and_valid_generator(valid_df, test_df, train_df, image_dir, x_col, y_cols, sample_size=100, batch_size=8, seed=1, target_w = 320, target_h = 320):
+   
+    # get generator to sample dataset
+    raw_train_generator = ImageDataGenerator().flow_from_dataframe(
+        dataframe=train_df, 
+        directory=IMAGE_DIR, 
+        x_col="Image", 
+        y_col=labels, 
+        class_mode="raw", 
+        batch_size=sample_size, 
+        shuffle=True, 
+        target_size=(target_w, target_h))
+    
+    
+    batch = raw_train_generator.next()
+    data_sample = batch[0]
+
+    # use sample to fit mean and std for test set generator
+    image_generator = ImageDataGenerator(
+        featurewise_center=True,
+        featurewise_std_normalization= True)
+    
+    # fit generator to sample from training data
+    image_generator.fit(data_sample)
+
+    # get test generator
+    valid_generator = image_generator.flow_from_dataframe(
+            dataframe=valid_df,
+            directory=image_dir,
+            x_col=x_col,
+            y_col=y_cols,
+            class_mode="raw",
+            batch_size=batch_size,
+            shuffle=False,
+            seed=seed,
+            target_size=(target_w,target_h))
+
+    test_generator = image_generator.flow_from_dataframe(
+            dataframe=test_df,
+            directory=image_dir,
+            x_col=x_col,
+            y_col=y_cols,
+            class_mode="raw",
+            batch_size=batch_size,
+            shuffle=False,
+            seed=seed,
+            target_size=(target_w,target_h))
+    return valid_generator, test_generator
+
+IMAGE_DIR = "C:/Users/dhruv/Desktop/cat/pic"  #Image Directory where Pic are saved
+train_generator = get_train_generator(train_df, IMAGE_DIR, "Image", labels)
+valid_generator, test_generator= get_test_and_valid_generator(valid_df, test_df, train_df, IMAGE_DIR, "Image", labels)
+
+#ConvNET 
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(320,320, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(320,320, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+model = model.output
+model = GlobalAveragePooling2D()(model)
+predictions = Dense(len(labels), activation="sigmoid")(models)
+
+opt = SGD(lr=0.001, momentum=0.9)
+model = Model(inputs=model.input, outputs=predictions)
+model.compile(optimizer= opt,  loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model.fit_generator(train_generator, 
+                              validation_data=test_generator,
+                              steps_per_epoch= 1000, 
+                              validation_steps= 200, 
+                              epochs = 12)
+
+# Plot the loss with each iteration
+plt.plot(history.history['loss'])
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.title("Training Loss Curve")
+plt.show()
+
+
+model.save_weights("model.h5")
+
+predicted_vals = model.predict_generator(test_generator, steps = len(test_generator))
+
+# Initialize an np array of zeros
+l= np.zeros((len(predicted_vals),1))
+
+# probability above 0.5 gives dog and less than gives cat
+for i in range(0,len(predicted_vals)):
+    if predicted_vals[i] >= 0.5 :
+        l[i] = 1
+    else:
+        l[i] = 0
+# checking Values in the confusion Matrix       
+y = test_df.iloc[:, 1].values
+cm = confusion_matrix(y,l)
+
+auc_rocs = util.get_roc_curve(labels, predicted_vals, test_generator)
+
+
+
