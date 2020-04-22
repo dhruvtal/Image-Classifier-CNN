7cbfdbc3d91122c85ccc9328537a9dc6c121e2af
Dhruv Talwar <64063264+dhruvtal@users.noreply.github.com>
2020-04-22 12:42:04 +0530

    Add files via upload

---
+++ util.py
@@ -0,0 +1,38 @@
+import random
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import backend as K
+from keras.preprocessing import image
+from sklearn.metrics import roc_auc_score, roc_curve
+from tensorflow.compat.v1.logging import INFO, set_verbosity
+
+random.seed(a=None, version=2)
+
+set_verbosity(INFO)
+
+def get_roc_curve(labels, predicted_vals, generator):
+    auc_roc_vals = []
+    for i in range(len(labels)):
+        try:
+            gt = generator.labels[:, i]
+            pred = predicted_vals[:, i]
+            auc_roc = roc_auc_score(gt, pred)
+            auc_roc_vals.append(auc_roc)
+            fpr_rf, tpr_rf, _ = roc_curve(gt, pred)
+            plt.figure(1, figsize=(10, 10))
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.plot(fpr_rf, tpr_rf,
+                     label=labels[i] + " (" + str(round(auc_roc, 3)) + ")")
+            plt.xlabel('False positive rate')
+            plt.ylabel('True positive rate')
+            plt.title('ROC curve')
+            plt.legend(loc='best')
+        except:
+            print(
+                f"Error in generating ROC curve for {labels[i]}. "
+                f"Dataset lacks enough examples."
+            )
+    plt.show()
+    return auc_roc_vals
